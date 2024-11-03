278443abfba68efb7b44c9b92fff8b50d1adca61
jyzxc <867137498@qq.com>
2024-11-04 02:04:54 +0800

    fix: fix the video play problem

--- app/src/main/java/com/caodong0225/videoplayer/MainActivity.kt
+++ app/src/main/java/com/caodong0225/videoplayer/MainActivity.kt
@@ -2,63 +2,47 @@ package com.caodong0225.videoplayer
 
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
-import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.util.EventLogger
+
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var videoView: VideoView
-    private lateinit var playButton: Button
-    private lateinit var pauseButton: Button
-    private lateinit var stopButton: Button
+    private lateinit var player: ExoPlayer
+    private lateinit var playerView: PlayerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // 初始化 VideoView 和按钮
-        videoView = findViewById(R.id.videoView)
-        playButton = findViewById(R.id.playButton)
-        pauseButton = findViewById(R.id.pauseButton)
-        stopButton = findViewById(R.id.stopButton)
+        playerView = findViewById(R.id.playerView)
 
-        // 设置视频的 URI，可以使用本地文件路径或网络URL
-        val videoUri: Uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.sample_video)
-        videoView.setVideoURI(videoUri)
+        // 初始化 ExoPlayer
+        player = ExoPlayer.Builder(this).build()
+        playerView.player = player
+        // 添加事件监听器以获取详细日志
+        // 添加事件监听器以获取详细日志
+        player.addAnalyticsListener(EventLogger())
 
-        // 播放按钮点击事件
-        playButton.setOnClickListener {
-            if (!videoView.isPlaying) {
-                videoView.start()
-            }
-        }
+        val videoUri = Uri.parse("android.resource://${packageName}/${R.raw.sample_video}")
+        val mediaItem = MediaItem.fromUri(videoUri)
 
-        // 暂停按钮点击事件
-        pauseButton.setOnClickListener {
-            if (videoView.isPlaying) {
-                videoView.pause()
-            }
-        }
+        player.setMediaItem(mediaItem)
+        player.prepare()
+        player.play();
 
-        // 停止按钮点击事件
-        stopButton.setOnClickListener {
-            if (videoView.isPlaying) {
-                videoView.stopPlayback()
-                videoView.resume()  // 让视频重新从头开始播放
-            }
-        }
     }
 
     override fun onPause() {
         super.onPause()
-        if (videoView.isPlaying) {
-            videoView.pause()  // 暂停视频播放
-        }
+        player.pause()  // 暂停播放
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        videoView.stopPlayback()  // 停止并释放资源
+        player.release()  // 释放资源
     }
 }
